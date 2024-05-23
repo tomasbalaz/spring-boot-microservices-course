6155f43a59820dd31d40ed7e8861c61ccf563b7f
Tomas Balaz <balaztomas0@gmail.com>
2024-05-23 15:07:36 +0200

    Created AbstractIntegrationTest

--- catalog-service/src/test/java/sk/balaz/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/sk/balaz/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
-@Import(ContainersConfig.class)
-class CatalogServiceApplicationTests {
+class CatalogServiceApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {}
