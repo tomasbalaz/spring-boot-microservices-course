07690a8271949955f5f3ea58c35abc942aa65971
Tomas Balaz <balaztomas0@gmail.com>
2024-05-31 15:17:51 +0200

    Created unit and integration test for order-service

--- order-service/src/test/java/sk/balaz/bookstore/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/sk/balaz/bookstore/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package sk.balaz.bookstore.orderservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OrderServiceApplicationTests {
+class OrderServiceApplicationTests extends AbstractIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
