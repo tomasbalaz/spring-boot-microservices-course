c6084caba11fc398884823cf452611a8adb64afd
Tomas Balaz <balaztomas0@gmail.com>
2024-05-31 12:54:51 +0200

    Created order-service application

---
+++ order-service/src/test/java/sk/balaz/bookstore/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,30 @@
+package sk.balaz.bookstore.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestOrderServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+	}
+
+	@Bean
+	@ServiceConnection
+	RabbitMQContainer rabbitContainer() {
+		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.13-alpine"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestOrderServiceApplication.class).run(args);
+	}
+
+}
